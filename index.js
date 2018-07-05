a239de0d9ddf4d1e9d67ca3543771130c95633b4
Michelle Olivieri <michelleo72@gmail.com>
2018-07-05 09:47:42 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('.ranked-list li');
+  for (let i = 0; i < lis.length; ++i) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  let deepest = document.querySelector('#grand-node div div div div');
+  return deepest;
+}
